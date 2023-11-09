289919c9afd92a3c05953690fd00ffd12093a342
Pop Razvan <razvanpopbm@yahoo.com>
2023-11-09 16:54:51 +0200

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
-  return (
-   <h1 className='text-4xl'>Home</h1>
-  )
+  return <h1 className="text-4xl">CineSphere</h1>;
 }
