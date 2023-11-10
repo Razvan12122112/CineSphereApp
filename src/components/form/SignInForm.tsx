54ee5ac535cccde09df59688d3b3240428c2cb02
Pop Razvan <razvanpopbm@yahoo.com>
2023-11-10 16:56:59 +0200

    Sing-in and sing-up form validation completed

--- src/components/form/SignInForm.tsx
+++ src/components/form/SignInForm.tsx
@@ -1,5 +1,95 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import Link from "next/link";
+import GoogleSignInButton from "../GoogleSignInButton";
+
+const FormSchema = z.object({
+  email: z.string().min(1, "Email is required").email("Invalid email"),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(8, "Password must have more than 8 characters"),
+});
+
 const SignInForm = () => {
-  return <div>Sign in</div>;
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof FormSchema>) => {
+    console.log("form submited");
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+        <div className="space-y-2">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="mail@example.com" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Enter your password"
+                    type="password"
+                    {...field}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <Button className="w-full mt-6" type="submit">
+          Sign in
+        </Button>
+      </form>
+      <div>
+        <p className="mt-1 w-full">
+          If you don't have an account please{" "}
+          <Link className="text-blue-500 hover:underline" href="/sign-up">
+            Sign up
+          </Link>
+        </p>
+        <GoogleSignInButton>Sign in with Google</GoogleSignInButton>
+      </div>
+    </Form>
+  );
 };
 
 export default SignInForm;
--- src/components/form/SignUPForm.tsx
+++ src/components/form/SignUPForm.tsx
@@ -1,5 +1,128 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import Link from "next/link";
+import GoogleSignInButton from "../GoogleSignInButton";
+
+const FormSchema = z
+  .object({
+    username: z.string().min(1, "Username is required").max(30),
+    email: z.string().min(1, "Email is required").email("Invalid email"),
+    password: z
+      .string()
+      .min(1, "Password is required")
+      .min(8, "Password must have more than 8 characters"),
+    confirmPassword: z.string().min(1, "Password confirmation is required"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Password do not match",
+  });
+
 const SignUpForm = () => {
-  return <div>Sign up</div>;
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof FormSchema>) => {
+    console.log("form submited");
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+        <div className="space-y-2">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="Username" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="mail@example.com" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Enter your password"
+                    type="password"
+                    {...field}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Re-enter your password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Re-enter your password"
+                    type="password"
+                    {...field}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <Button className="w-full mt-6" type="submit">
+          Sign up
+        </Button>
+      </form>
+      <div className="mt-3">
+        <GoogleSignInButton>Sign up with Google</GoogleSignInButton>
+      </div>
+    </Form>
+  );
 };
 
 export default SignUpForm;
